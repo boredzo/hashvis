5c688e10d5bd51c10c61abc70617d838431b4822
Peter Hosey <git@boredzo.org>
2015-12-02 22:02:48 -0800

    Wrap the bits that run when this program is run as an executable in “if __name__ == '__main__':”.

--- hashvis.py
+++ hashvis.py
@@ -117,52 +117,53 @@ def hash_to_pic(hash):
 		yield ''.join(output_chunks[:pixels_per_row]) + reset
 		del output_chunks[:pixels_per_row]
 
-run_tests = False
-if run_tests:
-	# A square number. Should contain a diagonal pair (in this case, (16,16)).
-	factors_of_256 = set(factors(256))
-	assert factors_of_256 == set([(256, 1), (16, 16), (8, 32), (2, 128), (64, 4), (1, 256), (32, 8), (128, 2), (4, 64)])
+if __name__ == '__main__':
+	run_tests = False
+	if run_tests:
+		# A square number. Should contain a diagonal pair (in this case, (16,16)).
+		factors_of_256 = set(factors(256))
+		assert factors_of_256 == set([(256, 1), (16, 16), (8, 32), (2, 128), (64, 4), (1, 256), (32, 8), (128, 2), (4, 64)])
 
-	# A rectangular number: not square, but still composite. No diagonal pair here.
-	factors_of_12 = set(factors(12))
-	assert factors_of_12 == set([(2, 6), (12, 1), (1, 12), (6, 2), (4, 3), (3, 4)])
+		# A rectangular number: not square, but still composite. No diagonal pair here.
+		factors_of_12 = set(factors(12))
+		assert factors_of_12 == set([(2, 6), (12, 1), (1, 12), (6, 2), (4, 3), (3, 4)])
 
-	assert (1, 256) in factors_of_256
-	assert (256, 1) in factors_of_256
-	assert (1, 256) not in except_one(factors_of_256)
-	assert (256, 1) not in except_one(factors_of_256)
+		assert (1, 256) in factors_of_256
+		assert (256, 1) in factors_of_256
+		assert (1, 256) not in except_one(factors_of_256)
+		assert (256, 1) not in except_one(factors_of_256)
 
-	# A prime number. Should have exactly one pair of factors.
-	factors_of_5 = set(factors(5))
-	assert factors_of_5 == set([(1, 5), (5, 1)])
+		# A prime number. Should have exactly one pair of factors.
+		factors_of_5 = set(factors(5))
+		assert factors_of_5 == set([(1, 5), (5, 1)])
 
-	assert list(parse_hex('ab15e')) == [0xab, 0x15, 0x0e]
-	assert list(parse_hex(':::ab:15:e')) == [0xab, 0x15, 0x0e]
+		assert list(parse_hex('ab15e')) == [0xab, 0x15, 0x0e]
+		assert list(parse_hex(':::ab:15:e')) == [0xab, 0x15, 0x0e]
 
-	assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.\n') == 'b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38'
-	assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.') == 'b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38'
-	#Alternate output example from https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Authentication_Keys :
-	assert extract_hash_from_line('RSA key fingerprint is MD5:10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16.') == '10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16'
-	# Also from https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Authentication_Keys :
-	assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n') == '2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n')
-	assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.') == '2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.')
+		assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.\n') == 'b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38'
+		assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.') == 'b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38'
+		#Alternate output example from https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Authentication_Keys :
+		assert extract_hash_from_line('RSA key fingerprint is MD5:10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16.') == '10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16'
+		# Also from https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Authentication_Keys :
+		assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n') == '2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n')
+		assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.') == '2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.')
 
-	assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49\n') == 'e21c7b846f76826d52a0ade79ef9cb49'
-	assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49') == 'e21c7b846f76826d52a0ade79ef9cb49'
-	assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py\n') == '8b948e9c85fdf68f872017d7064e839c'
-	assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py') == '8b948e9c85fdf68f872017d7064e839c'
-	assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py\n') == '2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924'
-	assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py') == '2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924'
-	assert extract_hash_from_line('#!/usr/bin/python\n') is None
+		assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49\n') == 'e21c7b846f76826d52a0ade79ef9cb49'
+		assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49') == 'e21c7b846f76826d52a0ade79ef9cb49'
+		assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py\n') == '8b948e9c85fdf68f872017d7064e839c'
+		assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py') == '8b948e9c85fdf68f872017d7064e839c'
+		assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py\n') == '2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924'
+		assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py') == '2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924'
+		assert extract_hash_from_line('#!/usr/bin/python\n') is None
 
-	sys.exit(0)
+		sys.exit(0)
 
-import fileinput
+	import fileinput
 
-for input_line in fileinput.input():
-	print input_line.rstrip('\n')
+	for input_line in fileinput.input():
+		print input_line.rstrip('\n')
 
-	hash = extract_hash_from_line(input_line)
-	if hash:
-		for output_line in hash_to_pic(hash):
-			print output_line
+		hash = extract_hash_from_line(input_line)
+		if hash:
+			for output_line in hash_to_pic(hash):
+				print output_line

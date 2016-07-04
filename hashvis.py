3b53b8499aaa7eb8c49110cfdf61a2068e72c0d8
Peter Hosey <git@boredzo.org>
2016-07-03 19:18:53 -0700

    Fix #5: Recognize whether input is in hex, and, if so, represent it as (bolded) hex.

--- hashvis.py
+++ hashvis.py
@@ -37,18 +37,19 @@ ECDSA_exp = re.compile(r'^ECDSA key fingerprint is SHA256:([+/0-9a-zA-Z]+)\.')
 more_base64_padding_than_anybody_should_ever_need = '=' * 64
 
 def extract_hash_from_line(input_line):
+	"Returns a tuple of the extracted hash as hex, and whether it was originally hex (vs, say, base64). The hash may be None if none was found in the input."
 	if input_line[:1] == 'M':
 		match = MD5_exp.match(input_line)
 		if match:
-			return match.group(1)
+			return match.group(1), True
 		else:
-			return ''
+			return '', False
 	elif input_line[:1] == 'R':
 		match = RSA_exp.match(input_line)
 		if match:
-			return match.group(1)
+			return match.group(1), True
 		else:
-			return ''
+			return '', False
 	elif input_line[:1] == 'E':
 		match = ECDSA_exp.match(input_line)
 		if match:
@@ -56,7 +57,7 @@ def extract_hash_from_line(input_line):
 			# Pacify the base64 module, which wants *some* padding (at least sometimes) but doesn't care how much.
 			b64str += more_base64_padding_than_anybody_should_ever_need
 			# Re-encode to hex for processing downstream. Arguably a refactoring opportunity…
-			return binascii.b2a_hex(base64.b64decode(b64str))
+			return binascii.b2a_hex(base64.b64decode(b64str)), False
 
 	if input_line:
 		try:
@@ -70,9 +71,9 @@ def extract_hash_from_line(input_line):
 			int(hash, 16)
 		except ValueError:
 			# Not a hex number.
-			return None
+			return None, False
 		else:
-			return hash
+			return hash, True
 
 def parse_hex(hex):
 	hex = hex.lstrip(':-')
@@ -80,7 +81,7 @@ def parse_hex(hex):
 		byte_hex, hex = hex[:2], hex[2:].lstrip(':-')
 		yield int(byte_hex, 16)
 
-def hash_to_pic(hash, only_ever_one_line=False):
+def hash_to_pic(hash, only_ever_one_line=False, represent_as_hex=False):
 	bytes = parse_hex(hash)
 	def fgcolor(idx):
 		idx = ((idx >> 4) & 0xf)
@@ -100,8 +101,9 @@ def hash_to_pic(hash, only_ever_one_line=False):
 			base = 100
 			idx = idx - 0x8
 		return '\x1b[{0}m'.format(base + idx)
+	bold = '\x1b[1m'
 	reset = '\x1b[0m'
-	characters = [
+	characters = list('0123456789abcdef') if represent_as_hex else [
 		'▚',
 		'▞',
 		'▀',
@@ -117,8 +119,13 @@ def hash_to_pic(hash, only_ever_one_line=False):
 	last_byte = 0
 	character_idx = None
 	for b in bytes:
-		character_idx = b % len(characters)
-		output_chunks.append(fgcolor(b) + bgcolor(b) + characters[character_idx])
+		def find_character(b):
+			character_idx = b % len(characters)
+			return characters[character_idx]
+		if not represent_as_hex:
+			output_chunks.append(fgcolor(b) + bgcolor(b) + find_character(b))
+		else:
+			output_chunks.append(fgcolor(b) + bgcolor(b) + find_character(b >> 4) + find_character(b & 0xf))
 		last_byte = b
 
 	if only_ever_one_line:
@@ -126,7 +133,7 @@ def hash_to_pic(hash, only_ever_one_line=False):
 	else:
 		pixels_per_row, num_rows = pairs[last_byte % len(pairs)]
 	while output_chunks:
-		yield ''.join(output_chunks[:pixels_per_row]) + reset
+		yield bold + ''.join(output_chunks[:pixels_per_row]) + reset
 		del output_chunks[:pixels_per_row]
 
 if __name__ == '__main__':
@@ -152,29 +159,30 @@ if __name__ == '__main__':
 		assert list(parse_hex('ab15e')) == [0xab, 0x15, 0x0e]
 		assert list(parse_hex(':::ab:15:e')) == [0xab, 0x15, 0x0e]
 
-		assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.\n') == 'b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38'
-		assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.') == 'b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38'
+		assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.\n') == ('b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38', True)
+		assert extract_hash_from_line('RSA key fingerprint is b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38.') == ('b8:79:03:7d:00:44:98:6e:67:a0:59:1a:01:21:36:38', True)
 		#Alternate output example from https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Authentication_Keys :
-		assert extract_hash_from_line('RSA key fingerprint is MD5:10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16.') == '10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16'
+		assert extract_hash_from_line('RSA key fingerprint is MD5:10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16.') == ('10:4a:ec:d2:f1:38:f7:ea:0a:a0:0f:17:57:ea:a6:16', True)
 		# Also from https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Authentication_Keys :
-		assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n') == '2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n')
-		assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.') == '2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.')
+		assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n') == ('2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', False), extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.\n')
+		assert extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.') == ('2cf162318aeb098415b1550fce3387bfe663cb10879556093015457ab5423fb9', False), extract_hash_from_line('ECDSA key fingerprint is SHA256:LPFiMYrrCYQVsVUPzjOHv+ZjyxCHlVYJMBVFerVCP7k.')
 		#UUID
-		assert extract_hash_from_line('E6CD379E-12CD-4E00-A83A-B06E74CF03B8') == 'E6CD379E12CD4E00A83AB06E74CF03B8', extract_hash_from_line('E6CD379E-12CD-4E00-A83A-B06E74CF03B8')
-		assert extract_hash_from_line('e6cd379e-12cd-4e00-a83a-b06e74cf03b8') == 'e6cd379e12cd4e00a83ab06e74cf03b8', extract_hash_from_line('e6cd379e-12cd-4e00-a83a-b06e74cf03b8')
+		assert extract_hash_from_line('E6CD379E-12CD-4E00-A83A-B06E74CF03B8') == ('E6CD379E12CD4E00A83AB06E74CF03B8', True), extract_hash_from_line('E6CD379E-12CD-4E00-A83A-B06E74CF03B8')
+		assert extract_hash_from_line('e6cd379e-12cd-4e00-a83a-b06e74cf03b8') == ('e6cd379e12cd4e00a83ab06e74cf03b8', True), extract_hash_from_line('e6cd379e-12cd-4e00-a83a-b06e74cf03b8')
 
-		assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49\n') == 'e21c7b846f76826d52a0ade79ef9cb49'
-		assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49') == 'e21c7b846f76826d52a0ade79ef9cb49'
-		assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py\n') == '8b948e9c85fdf68f872017d7064e839c'
-		assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py') == '8b948e9c85fdf68f872017d7064e839c'
-		assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py\n') == '2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924'
-		assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py') == '2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924'
-		assert extract_hash_from_line('#!/usr/bin/python\n') is None
+		assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49\n') == ('e21c7b846f76826d52a0ade79ef9cb49', True)
+		assert extract_hash_from_line('MD5 (hashvis.py) = e21c7b846f76826d52a0ade79ef9cb49') == ('e21c7b846f76826d52a0ade79ef9cb49', True)
+		assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py\n') == ('8b948e9c85fdf68f872017d7064e839c', True)
+		assert extract_hash_from_line('8b948e9c85fdf68f872017d7064e839c  hashvis.py') == ('8b948e9c85fdf68f872017d7064e839c', True)
+		assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py\n') == ('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924', True)
+		assert extract_hash_from_line('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924  hashvis.py') == ('2c9997ce32cb35823b2772912e221b350717fcb2d782c667b8f808be44ae77ba1a7b94b4111e386c64a2e87d15c64a2fc2177cd826b9a0fba6b348b4352ed924', True)
+		assert extract_hash_from_line('#!/usr/bin/python\n')[0] is None
 
-		(line,) = hash_to_pic('78')
-		assert line == '\x1b[37m\x1b[100m\xe2\x96\x9a\x1b[0m', repr(line)
-		(line,) = hash_to_pic('7f')
-		assert line == '\x1b[37m\x1b[107m\xe2\x96\x8c\x1b[0m', repr(line)
+		# Protip: Use vis -co to generate these.
+		(line,) = hash_to_pic('78', represent_as_hex=True)
+		assert line == '\033[1m\033[37m\033[100m78\033[0m', repr(line)
+		(line,) = hash_to_pic('7f', represent_as_hex=True)
+		assert line == '\033[1m\033[37m\033[107m7f\033[0m', repr(line)
 		assert list(hash_to_pic('aebece')) != list(hash_to_pic('deeefe')), (list(hash_to_pic('aebece')), list(hash_to_pic('deeefe')))
 		assert list(hash_to_pic('eaebec')) != list(hash_to_pic('edeeef')), (list(hash_to_pic('eaebec')), list(hash_to_pic('edeeef')))
 		sys.exit(0)
@@ -188,7 +196,7 @@ if __name__ == '__main__':
 	for input_line in fileinput.input(args):
 		print input_line.rstrip('\n')
 
-		hash = extract_hash_from_line(input_line)
+		hash, is_hex = extract_hash_from_line(input_line)
 		if hash:
-			for output_line in hash_to_pic(hash, only_ever_one_line=options.one_line):
+			for output_line in hash_to_pic(hash, only_ever_one_line=options.one_line, represent_as_hex=is_hex):
 				print output_line
